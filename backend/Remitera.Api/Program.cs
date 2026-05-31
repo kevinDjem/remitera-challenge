d63839ff3ec485c4a07af20def37c706567ea8c4
Kevin Djemdjemian <kfdjem@gmail.com>
2026-05-31 15:01:00 -0300

    chore: support database connection from environment variables

--- backend/Remitera.Api/Program.cs
+++ backend/Remitera.Api/Program.cs
@@ -11,9 +11,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IRemitoService, RemitoService>();
 
+var connectionString =
+    Environment.GetEnvironmentVariable("DATABASE_URL")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseNpgsql(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddCors(options =>
 {
